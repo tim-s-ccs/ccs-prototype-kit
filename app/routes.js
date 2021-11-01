c5495d4db91870554d8eabc6ea56831170df67a2
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-11-01 15:37:26 +0000

    Add example page for CCS

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,7 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/ccs/example/home', (_, res) => {
+  res.render('ccs/example/index.html')
+})
